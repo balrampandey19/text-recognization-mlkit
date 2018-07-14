7065a13a671eb66d89bbc3c8a0e5f0b62c830786
Balram Pandey <balrampandey19@gmail.com>
2018-07-14 21:35:09 +0530

    Initial commit

---
+++ app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
@@ -0,0 +1,12 @@
+package balrampandey19.com.text_recognization_mlkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
