8cf14e0d65fefb8a6a8aa11758d5c4ec4494a7a6
Balram Pandey <balrampandey19@gmail.com>
2018-07-16 23:09:55 +0530

    Capture image

--- app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
+++ app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
@@ -2,11 +2,16 @@ package balrampandey19.com.text_recognization_mlkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.provider.MediaStore
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val requestCode = 20
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,10 @@ class MainActivity : AppCompatActivity() {
         val image = findViewById<ImageView>(R.id.captured_photo)
         val capture = findViewById<Button>(R.id.photo_button)
 
+        capture.setOnClickListener(View.OnClickListener {
+            val photoCaptureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(photoCaptureIntent, requestCode)
 
+        })
     }
 }
