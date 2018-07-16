3ff81715440674789bf989db1030251ac9458ccb
Balram Pandey <balrampandey19@gmail.com>
2018-07-16 22:59:31 +0530

    Ui fix

--- app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
+++ app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
@@ -2,11 +2,20 @@ package balrampandey19.com.text_recognization_mlkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val onDevice = findViewById<Button>(R.id.device)
+        val onCloud = findViewById<Button>(R.id.cloude)
+        val image = findViewById<ImageView>(R.id.captured_photo)
+        val capture = findViewById<Button>(R.id.photo_button)
+
+
     }
 }
