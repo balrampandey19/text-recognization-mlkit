65b208f6c0c555691e79a161447bf59796bba2f8
Balram Pandey <balrampandey19@gmail.com>
2018-07-17 23:44:44 +0530

    Process Text Recognition on edittext

--- app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
+++ app/src/main/java/balrampandey19/com/text_recognization_mlkit/MainActivity.kt
@@ -65,4 +65,24 @@ class MainActivity : AppCompatActivity() {
                             }
                         })
     }
+
+    private fun processTextRecognitionResult(texts: FirebaseVisionText) {
+        val blocks = texts.blocks
+        if (blocks.size == 0) {
+            showToast("No text found")
+            return
+        }
+        mGraphicOverlay.clear()
+        for (i in blocks.indices) {
+            val lines = blocks[i].lines
+            for (j in lines.indices) {
+                val elements = lines[j].elements
+                for (k in elements.indices) {
+                    val textGraphic = TextGraphic(mGraphicOverlay, elements[k])
+                    mGraphicOverlay.add(textGraphic)
+
+                }
+            }
+        }
+    }
 }
